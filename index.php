f945e7000a7e66c8b0fd102d0ce3c5d9fa5c2463
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-06-10 02:37:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,9 @@ echo 'Commit 2 de prueba';
 echo '<h1> estoy desplegado desde GH</h1>';
 
 echo '<a href="https://www.facebook.com/">Ir a Facebook</a>';
+
+
+echo '<a href="/tests/getxy.php">Ir a getXY</a>';
+echo '<a href="/tests/recibe.php">Ir a recibe</a>';
+echo '<a href="/tests/testsession1.php">Ir a session1</a>';
+echo '<a href="/tests/timer1.php">Ir a timer1</a>';
